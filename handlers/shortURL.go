5b1f67a4702278d7f3c62b9cd09e1dcbac3bc991
Keynandz <ndhsti2980@gmail.com>
2023-11-20 16:58:47 +0700

    feat: Add handlers and views

---
+++ handlers/shortURL.go
@@ -0,0 +1,60 @@
+package handlers
+
+import (
+	"fmt"
+	"go-shorturl/repositories"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+var urlMap = make(map[string]string)
+
+type ShortenRequest struct {
+	LongURL        string `json:"longURL"`
+	CustomShortURL string `json:"customShortURL"`
+}
+
+type ShortenResponse struct {
+	ShortURL string `json:"shortURL"`
+}
+
+func ShortUrl(e *echo.Echo) error {
+	e.POST("keynandz/shorten", func(c echo.Context) error {
+		req := new(ShortenRequest)
+		if err := c.Bind(req); err != nil {
+			return c.String(http.StatusBadRequest, "Invalid request body")
+		}
+
+		var shortURL string
+		if req.CustomShortURL != "" {
+			shortURL = req.CustomShortURL
+		} else {
+			shortURL = repositories.GenerateShortURL()
+		}
+
+		urlMap[shortURL] = req.LongURL
+
+		format := "http://%s:%s/keynandz/%s"
+		response := fmt.Sprintf(format, os.Getenv("HOST"), os.Getenv("PORT"), shortURL)
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"short link":  response,
+			"status_code": http.StatusOK,
+		})
+	})
+
+	e.GET("keynandz/:shortURL", func(c echo.Context) error {
+		shortURL := c.Param("shortURL")
+		longURL, exists := urlMap[shortURL]
+		if exists {
+			return c.Redirect(http.StatusMovedPermanently, longURL)
+		}
+		return c.JSON(http.StatusNotFound, map[string]interface{}{
+			"message": "Short URL not found",
+			"error":   http.StatusNotFound,
+		})
+	})
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"go-shorturl/repositories"
+	"go-shorturl/handlers"
 	"log"
-	"net/http"
-	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
-
-var urlMap = make(map[string]string)
-
-type ShortenRequest struct {
-	LongURL         string `json:"longURL"`
-	CustomShortURL  string `json:"customShortURL"`
-}
-
-type ShortenResponse struct {
-	ShortURL string `json:"shortURL"`
-}
 
 func main() {
 	e := echo.New()
@@ -29,43 +15,8 @@ func main() {
 		log.Fatal("error loading file .env")
 	}
 
-	e.POST("keynandz/shorten", func(c echo.Context) error {
-		req := new(ShortenRequest)
-		if err := c.Bind(req); err != nil {
-			return c.String(http.StatusBadRequest, "Invalid request body")
-		}
-
-		var shortURL string
-		if req.CustomShortURL != "" {
-			shortURL = req.CustomShortURL
-		} else {
-			shortURL = repositories.GenerateShortURL()
-		}
-
-		urlMap[shortURL] = req.LongURL
-
-		format := "http://%s:%s/keynandz/%s"
-		response := fmt.Sprintf(format, os.Getenv("HOST"), os.Getenv("PORT"), shortURL)
-		return c.JSON(http.StatusOK, map[string]interface{}{
-			"short link":  response,
-			"status_code": http.StatusOK,
-		})
-	})
-
-	e.GET("keynandz/:shortURL", func(c echo.Context) error {
-		shortURL := c.Param("shortURL")
-		longURL, exists := urlMap[shortURL]
-		if exists {
-			return c.Redirect(http.StatusMovedPermanently, longURL)
-		}
-		return c.JSON(http.StatusNotFound, map[string]interface{}{
-			"message": "Short URL not found",
-			"error":   http.StatusNotFound,
-		})
-	})
-
+	handlers.ShortUrl(e)
 	e.Static("/", "views")
 	e.File("/index", "views/index.html")
-
 	e.Logger.Fatal(e.Start(":9090"))
 }
