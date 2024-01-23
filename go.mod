847588d869d96e8cfbcf4a49e10b688e0dd62b2e
Keynandz <ndhsti2980@gmail.com>
2024-01-23 16:13:10 +0700

    refactor: env

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module go-shorturl
 
 go 1.20
 
-require (
-	github.com/joho/godotenv v1.5.1
-	github.com/labstack/echo/v4 v4.11.3
-)
+require github.com/labstack/echo/v4 v4.11.3
 
 require (
 	github.com/labstack/gommon v0.4.1 // indirect
--- handlers/shortURL.go
+++ handlers/shortURL.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"go-shorturl/repositories"
 	"net/http"
-	"os"
 
 	"github.com/labstack/echo/v4"
 )
@@ -37,7 +36,7 @@ func ShortUrl(e *echo.Echo) error {
 		urlMap[shortURL] = req.LongURL
 
 		format := "http://%s:%s/keynandz/%s"
-		response := fmt.Sprintf(format, "keynandz-url.netlify.app", os.Getenv("PORT"), shortURL)
+		response := fmt.Sprintf(format, "keynandz-url.netlify.app", "9090", shortURL)
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"short link":  response,
 			"status_code": http.StatusOK,
